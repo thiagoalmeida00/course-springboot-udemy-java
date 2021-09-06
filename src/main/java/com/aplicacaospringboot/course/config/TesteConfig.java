4e9739d2af26e2d639e3ac968062fc9162005570
Thiago Almeida <t.leandroalmeida@gmail.com>
2021-09-06 14:15:04 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/aplicacaospringboot/course/config/TesteConfig.java
+++ src/main/java/com/aplicacaospringboot/course/config/TesteConfig.java
@@ -22,9 +22,6 @@ import com.aplicacaospringboot.course.repositories.UserRepository;
 @Profile("test")
 public class TesteConfig implements CommandLineRunner {
 
-	// mecanismo de injeção de dependência automático
-	// associar uma instância do UserRepository no TestConfig
-
 	@Autowired
 	private UserRepository userRepository;
 
@@ -37,7 +34,6 @@ public class TesteConfig implements CommandLineRunner {
 	@Autowired
 	private ProductRepository productRepository;
 	
-	// executar quando a aplicação for iniciada
 	@Override
 	public void run(String... args) throws Exception {
 
@@ -51,6 +47,19 @@ public class TesteConfig implements CommandLineRunner {
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, ""); 
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
@@ -58,17 +67,10 @@ public class TesteConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
-		// salva uma lista de objetos (criada no parâmetro)
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
-		
-		
+				
 	}
 
 }
--- src/main/java/com/aplicacaospringboot/course/entities/Product.java
+++ src/main/java/com/aplicacaospringboot/course/entities/Product.java
@@ -9,6 +9,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -17,7 +20,7 @@ import javax.persistence.Transient;
 public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -25,14 +28,14 @@ public class Product implements Serializable {
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	// impede que o JPA interprete(provisório)
-	@Transient
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
 	}
-	
+
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		this.id = id;
 		this.name = name;
@@ -101,5 +104,5 @@ public class Product implements Serializable {
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
