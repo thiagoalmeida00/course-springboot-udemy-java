5b58865895b1148aae78beb4e09cb1990cc52f71
Thiago Almeida <t.leandroalmeida@gmail.com>
2021-09-06 10:29:11 -0300

    OrderStatus enum]

--- src/main/java/com/aplicacaospringboot/course/entities/Order.java
+++ src/main/java/com/aplicacaospringboot/course/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.aplicacaospringboot.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
@@ -27,6 +28,9 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 
+	
+	private Integer orderStatus;
+	
 	// associação muitos para um (Chave estrangeira)
 
 	@ManyToOne
@@ -36,9 +40,10 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -56,6 +61,16 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
---
+++ src/main/java/com/aplicacaospringboot/course/entities/enums/OrderStatus.java
@@ -0,0 +1,31 @@
+package com.aplicacaospringboot.course.entities.enums;
+
+public enum OrderStatus {
+
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(5);
+
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	// método stático para converter um valor numérico para tipoEnumerado
+	public static OrderStatus valueOf(int code) {
+		for (OrderStatus value : OrderStatus.values()) {
+			if (value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Invalid OrderStatus code");
+	}
+	
+}
