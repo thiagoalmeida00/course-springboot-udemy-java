0682722ed44c7003c497111b1ef85ceb30492115
Thiago Almeida <t.leandroalmeida@gmail.com>
2021-09-16 08:56:51 -0300

    Exception Handler - findByIdh

--- src/main/java/com/aplicacaospringboot/course/services/UserService.java
+++ src/main/java/com/aplicacaospringboot/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.aplicacaospringboot.course.entities.User;
 import com.aplicacaospringboot.course.repositories.UserRepository;
+import com.aplicacaospringboot.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); // se ao tentar dar o get, se não tiver usuário lança uma exceção
 	}
 	
 	public User insert(User obj) {
