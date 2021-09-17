7a11083ee883273412b4a4d45c7950c1a72cfe4f
Thiago Almeida <t.leandroalmeida@gmail.com>
2021-09-17 08:31:06 -0300

    Exception handler - delete

--- src/main/java/com/aplicacaospringboot/course/services/UserService.java
+++ src/main/java/com/aplicacaospringboot/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.aplicacaospringboot.course.entities.User;
 import com.aplicacaospringboot.course.repositories.UserRepository;
+import com.aplicacaospringboot.course.services.exceptions.DataBaseException;
 import com.aplicacaospringboot.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 	
 	// getOne instancia um usuário, deixa um objeto monitorado pelo JPA para em seguida efetuar a operação com o DB
